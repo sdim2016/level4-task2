4ce95e5103522339ba3148704947258236d9dd38
Dmitrii Slepnev <odmsl002@edu.xamk.fi>
2020-03-07 17:48:06 +0100

    Attached the adapter to the RV + added getGameHistory()

--- app/src/main/java/com/slepnev/rockpaperscissors/ui/GameHistoryActivity.kt
+++ app/src/main/java/com/slepnev/rockpaperscissors/ui/GameHistoryActivity.kt
@@ -5,17 +5,36 @@ import android.view.Menu
 import android.view.MenuItem
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.slepnev.rockpaperscissors.R
+import com.slepnev.rockpaperscissors.database.GameRepository
+import com.slepnev.rockpaperscissors.model.Game
 
 import kotlinx.android.synthetic.main.activity_game_history.*
+import kotlinx.android.synthetic.main.content_game_history.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class GameHistoryActivity : AppCompatActivity() {
+
+    private val games = arrayListOf<Game>()
+    private val gameAdapter = GameAdapter(games)
+    private lateinit var gameRepository: GameRepository
+    private val mainScope = CoroutineScope(Dispatchers.Main)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_history)
         setSupportActionBar(toolbar)
 
+        gameRepository = GameRepository(this)
+
+        initViews()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -31,6 +50,24 @@ class GameHistoryActivity : AppCompatActivity() {
         return when (item.itemId) {
             R.id.action_history -> true
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun initViews() {
+        rvHistory.layoutManager = LinearLayoutManager(this@GameHistoryActivity, RecyclerView.VERTICAL, false)
+        rvHistory.adapter = gameAdapter
+        rvHistory.addItemDecoration(DividerItemDecoration(this@GameHistoryActivity, DividerItemDecoration.VERTICAL))
+        getGameHistory()
+    }
+
+    private fun getGameHistory() {
+        mainScope.launch {
+            val gameList = withContext(Dispatchers.IO) {
+                gameRepository.getAllGames()
+            }
+            this@GameHistoryActivity.games.clear()
+            this@GameHistoryActivity.games.addAll(gameList)
+            this@GameHistoryActivity.gameAdapter.notifyDataSetChanged()
         }
     }
 
