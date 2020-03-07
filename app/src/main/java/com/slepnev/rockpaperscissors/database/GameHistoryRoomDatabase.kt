ea55300d36648a1512a35f340655b9f1c7b04d07
Dmitrii Slepnev <odmsl002@edu.xamk.fi>
2020-03-07 13:42:50 +0100

    Added room database declaration

---
+++ app/src/main/java/com/slepnev/rockpaperscissors/database/GameHistoryRoomDatabase.kt
@@ -0,0 +1,36 @@
+package com.slepnev.rockpaperscissors.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.slepnev.rockpaperscissors.model.Game
+
+@Database(entities = [Game::class], version = 1, exportSchema = false)
+public abstract class GameHistoryRoomDatabase : RoomDatabase() {
+
+    abstract fun gameDao(): GameDao
+
+    companion object {
+        private const val DATABASE_NAME = "GAME_HISTORY_DB"
+
+        @Volatile
+        private var gameHistoryRoomDatabaseInstance: GameHistoryRoomDatabase? = null
+
+        fun getDatabase(context: Context): GameHistoryRoomDatabase? {
+            if (gameHistoryRoomDatabaseInstance == null) {
+                synchronized(GameHistoryRoomDatabase::class.java) {
+                    if (gameHistoryRoomDatabaseInstance == null) {
+                        gameHistoryRoomDatabaseInstance = Room.databaseBuilder(context.applicationContext,
+                            GameHistoryRoomDatabase::class.java,
+                            DATABASE_NAME).build()
+                    }
+                }
+            }
+
+            return gameHistoryRoomDatabaseInstance
+        }
+    }
+
+
+}
